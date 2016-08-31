37cb376b832abdcae2cb309251493a841450f1d6
wujiang369 <360692545@qq.com>
2016-08-31 16:22:07 +0800

    web html
    html+js+css

---
+++ htdocs/js/add.js
@@ -0,0 +1,50 @@
+
+function addOne(){
+	var showControl = document.getElementById("txtNumber");
+	showControl.value = parseInt(showControl.value) + 1;
+},
+
+function removeOne(){
+	var showControl = document.getElementById("txtNumber");
+	showControl.value = parseInt(showControl.value) - 1;
+},
+
+function addOne1(){
+	var showControl = document.getElementById("txtNumber1");
+	showControl.value = parseInt(showControl.value) + 1;
+},
+
+function removeOne1(){
+	var showControl = document.getElementById("txtNumber1");
+	showControl.value = parseInt(showControl.value) - 1;
+},
+
+function addOne2(){
+	var showControl = document.getElementById("txtNumber2");
+	showControl.value = parseInt(showControl.value) + 1;
+},
+
+function removeOne2(){
+	var showControl = document.getElementById("txtNumber2");
+	showControl.value = parseInt(showControl.value) - 1;
+},
+
+function addOne3(){
+	var showControl = document.getElementById("txtNumber3");
+	showControl.value = parseInt(showControl.value) + 1;
+},
+
+function removeOne3(){
+	var showControl = document.getElementById("txtNumber3");
+	showControl.value = parseInt(showControl.value) - 1;
+},
+
+function addOne4(){
+	var showControl = document.getElementById("txtNumber4");
+	showControl.value = parseInt(showControl.value) + 1;
+},
+
+function removeOne4(){
+	var showControl = document.getElementById("txtNumber4");
+	showControl.value = parseInt(showControl.value) - 1;
+}
